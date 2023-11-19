e1402ef76101a2cc58cb8b01edabc067cc407e69
Marcel Kollovieh <ga94hej@mytum.de>
2023-11-19 15:51:47 +0100

    Updated logdir

--- seml_util/utils.py
+++ seml_util/utils.py
@@ -4,7 +4,12 @@ import os
 def create_logdir(logdir, ex):
     array_job_id = os.environ.get("SLURM_ARRAY_JOB_ID")
     array_task_id = os.environ.get("SLURM_ARRAY_TASK_ID")
-    logdir = os.path.join(logdir, f"{array_job_id}_{array_task_id}")
+    db_collection = ex.current_run.config["db_collection"]
+    if array_job_id is None:
+        ex.current_run._id
+        logdir = os.path.join(logdir, f"{db_collection}_{ex.current_run._id}")
+    else:
+        logdir = os.path.join(logdir, f"{db_collection}_{array_job_id}_{array_task_id}")
     os.makedirs(logdir, exist_ok=True)
     ex.current_run.config["logdir"] = logdir
     return logdir
